6d36d60fb4bc9c148c0e93caf472077c27be022d
HamzaKamal233 <kamal.hamza@outlook.com>
2024-06-23 23:52:57 -0700

    added database for products and reviews

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+# custom user model
 class customUserManager(BaseUserManager):
     
     def create_user(self, email, username, password, **kwargs):
@@ -47,4 +50,20 @@ class customUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-    
+
+ 
+# product database
+class product(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products/')
+
+# review database
+class review(models.Model):
+    user = models.ForeignKey(customUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.get_username()}'s review of {self.product.title}"
